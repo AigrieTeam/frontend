d3c1dd8b1fef42e09733591c5b2bcaf1d5eb5d4b
Noémie Tandol <noemie.tandol@gmail.com>
2023-12-07 23:20:47 +0100

    feat(page-jeu): front principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageJeuComponent } from './page-jeu/page-jeu.component';
 import { CarteJeuComponent } from './carte-jeu/carte-jeu.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageJeuComponent,
-    CarteJeuComponent
+    CarteJeuComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
