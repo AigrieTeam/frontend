dbc228bbc2ded84abed233458e9be418a8095365
Drosscend <47970260+Drosscend@users.noreply.github.com>
2023-12-07 23:20:48 +0100

    Easter egg 1 : konami code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  keySequence: string[] = [];
+  keySequenceString: string[] = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+  // ↑ ↑ ↓ ↓ ← → ← → b a
+  keySequenceStringIndex = 0;
+
+  keySequenceTrigger() {
+    document.addEventListener('keydown', (event) => {
+      this.keySequence.push(event.key);
+      if (this.keySequenceString[this.keySequenceStringIndex] === event.key) {
+        this.keySequenceStringIndex++;
+      } else {
+        this.keySequenceStringIndex = 0;
+      }
+      if (this.keySequenceStringIndex === this.keySequenceString.length) {
+        this.keySequenceStringIndex = 0;
+        this.keySequence = [];
+        this.trigger();
+      }
+    });
+  }
+
+  trigger() {
+    window.open('https://reseauactionclimat.org/urgence-climatique/', '_blank');
+  }
+
+  ngOnInit() {
+    this.keySequenceTrigger();
+  }
 }
